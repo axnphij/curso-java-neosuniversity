c7df27db35d6faa2b3a013daf84b1cb1dbaad058
axnphij <axnphij@gmail.com>
2019-03-20 11:53:14 -0600

    add commentos for a variable

--- src/com/neosuniversity/condiciones/QuizIf.java
+++ src/com/neosuniversity/condiciones/QuizIf.java
@@ -4,9 +4,11 @@ public class QuizIf {
     public static void main(String[] args) {
         int a = 67;
         if (a > 56 ){
-
+            System.out.println("valor de a= "= a); 
 
         }else {
+             System.out.println("valor de a= "= ai);
+
 
 
         }
